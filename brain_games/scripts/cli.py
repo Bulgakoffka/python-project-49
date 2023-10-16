ac096429a52ab67b4200be1aa6d04a93c7c66101
Stephen <146680209+Bulgakoffka@users.noreply.github.com>
2023-10-16 23:17:20 +0400

    autotest take three
    update cli.py

--- brain_games/scripts/cli.py
+++ brain_games/scripts/cli.py
@@ -4,5 +4,13 @@ def welcome_user():
     name = input('May I have your name? ')
     return name
 
+def greet():
+    print('Welcome to the Brain Games!')
 
+def main():
+    greet()
+    welcome_user()
+
+if __name__ == '__main__':
+    main()
 
