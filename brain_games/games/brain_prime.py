9dc57137139ccc4d034cfa1b0f8025b1bb6e6c87
Stephen <146680209+Bulgakoffka@users.noreply.github.com>
2025-03-11 02:08:31 +0400

    add brain prime game

---
+++ brain_games/games/brain_prime.py
@@ -0,0 +1,28 @@
+from random import randint, choice
+from brain_games.brain_engine.brain_games_engine import brain_engine
+
+
+def prime_game():
+    task = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+    def get_expression():
+        global expression
+        expression = randint(1, 101)
+        return expression
+    def correct_answer(expression):
+        for i in range(1, expression):
+            if i != 1 and expression % i == 0:
+                return 'no'
+        return 'yes'
+
+    brain_engine(task, get_expression, correct_answer)
+
+
+
+
+
+def main():
+    calc_game()
+
+
+if __name__ == '__main__':
+    main()
