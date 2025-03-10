aa9aa2e7f1d0b19eaf322563e9d17ef5ea1fe97b
Stephen <146680209+Bulgakoffka@users.noreply.github.com>
2025-03-10 21:35:57 +0400

    gcd game add

---
+++ brain_games/games/brain_gcd.py
@@ -0,0 +1,30 @@
+from random import randint, choice
+from brain_games.brain_engine.brain_games_engine import brain_engine
+
+
+def calc_game():
+    task = 'Find the greatest common divisor of given numbers.'
+    def get_expression():
+        global int1, int2
+        int1 = randint(1, 51)
+        int2 = randint(1, 51)
+        expression = f'{int1} {int2}'
+        return expression
+    def correct_answer(expression):
+        int1, int2 = expression.split()
+        if int1 > int2:
+            return int1
+        else:
+            return int2
+    brain_engine(task, get_expression, correct_answer)
+
+
+
+
+
+def main():
+    calc_game()
+
+
+if __name__ == '__main__':
+    main()
